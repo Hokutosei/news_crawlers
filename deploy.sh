83575f3f90402c4638a56ded26edfa7cb9c1ded2
jeanepaul <jeanepaul@gmail.com>
2015-07-14 16:20:48 +0900

    update some bug fixes

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,9 @@ docker build -t jeanepaul/news_crawlers .
 # echo "--->> pushing container"
 # docker push jeanepaul/news_crawlers:latest
 
+echo "--->> re-tag container..."
+docker tag -f jeanepaul/news_crawlers gcr.io/chat-app-proto01/news_crawlers
+
 echo "--->> pushing container"
 gcloud docker push gcr.io/chat-app-proto01/news_crawlers
 
@@ -18,6 +21,8 @@ kubectl stop pod newscrawlers
 echo "--->> creating newscrawlers pod"
 kubectl create -f "$(pwd)"/kubernets_pod.yaml
 
-echo "done!"
+echo "done! ctrl+c to stop status!"
+kubectl logs -f newscrawlers
+while true; do kubectl get pods; sleep 5; done
 # run container
 # docker run -d -e "COREOS_PRIVATE_IPV4=" jeanepaul/news_crawlers
